919e459f13c5e7bd35fb20b3c1c711d3eeb23be3
Sakurai07 <cool5tarXV@gmail.com>
2021-02-23 18:14:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ sleep 5
 nano ~/.bashrc
 cp lapt aptl
 cp aptl /bin/
+rm -rf aptl
 touch logs.txt
 echo 'Installed successfully' > logs.txt
 aptl install apt
 clear
-echo "done"
+echo "done, run aptl to start"
